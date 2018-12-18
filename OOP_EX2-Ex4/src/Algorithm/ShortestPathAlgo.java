586b79a572d58b2de8b37ea707151d9c42fcc953
Natalie <Natalie@NAT-LENW10>
2018-12-18 11:53:02 +0200

    add path- not comlete

--- OOP_EX2-Ex4/src/Algorithm/ShortestPathAlgo.java
+++ OOP_EX2-Ex4/src/Algorithm/ShortestPathAlgo.java
@@ -1,5 +1,17 @@
 package Algorithm;
 
-public class ShortestPathAlgo {
+import java.util.HashSet;
+
+import GIS.Game;
+import GIS.Path;
+import GIS.fruit;
+
+public class ShortestPathAlgo extends HashSet<Path> {
+	
+	HashSet<Path> Paths = new HashSet<>();
+	
+	public ShortestPathAlgo(Game game) {
+		double time= 
+	}
 
 }
--- OOP_EX2-Ex4/src/GIS/Path.java
+++ OOP_EX2-Ex4/src/GIS/Path.java
@@ -1,5 +1,28 @@
 package GIS;
 
-public class Path {
+import java.util.HashSet;
+import java.util.Iterator;
 
+import Geom.Point3D;
+
+public class Path extends HashSet<Point3D> {
+
+	HashSet<Point3D> path = new HashSet<>();
+	
+	public Path() {
+	}
+
+	public double length() {
+		double sum=0;
+		Iterator<Point3D> it= path.iterator();	
+		Point3D p= it.next(); 
+		
+		while(it.hasNext())   // while the layer has more elements
+		{
+			Point3D p2= it.next();
+			sum+= p.distance2D(p2);
+			p= p2;
+		}
+		return sum;
+	}
 }
