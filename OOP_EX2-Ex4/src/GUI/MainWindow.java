8ccc097902a5fd8b21fb38c06577fd90b292e7f9
micha <micha@LAPTOP-DHSIGLLP>
2018-12-18 18:33:38 +0200

    gui window changes

--- OOP_EX2-Ex4/src/GUI/MainWindow.java
+++ OOP_EX2-Ex4/src/GUI/MainWindow.java
@@ -15,6 +15,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -22,158 +23,233 @@ import java.util.Iterator;
 import javax.imageio.ImageIO;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
+import GIS.Game;
+import GIS.Map;
+import GIS.Packman;
+import GIS.fruit;
 import Geom.Point3D;
 
 
 public class MainWindow extends JFrame implements MouseListener
 {
 	public BufferedImage myImage;
-	ArrayList<Point3D> pacman = new ArrayList<Point3D>();
-	ArrayList<Point3D> fruit = new ArrayList<Point3D>();
+	//	ArrayList<Point3D> pacman = new ArrayList<Point3D>();
+	//	ArrayList<Point3D> fruit = new ArrayList<Point3D>();
 	int choice = 0 ;
+	Game game;
+	
 
 	public MainWindow() 
 	{
 		initGUI();		
 		this.addMouseListener(this); 
-    }
-		
+	}
+
 	private void initGUI() 
 	{
 		MenuBar menuBar = new MenuBar();
 		Menu menu = new Menu("Menu"); 
 		MenuItem item1 = new MenuItem("Pacman");
 		MenuItem item2 = new MenuItem("Fruit");
-		
+		MenuItem item3 = new MenuItem("Play");
+		MenuItem item4 = new MenuItem("Clear");
+		MenuItem item5 = new MenuItem("Load file");
+
 		menuBar.add(menu);
 		menu.add(item1);
 		menu.add(item2);
+		menu.add(item3);
+		menu.add(item4);
+		menu.add(item5);
 		this.setMenuBar(menuBar);
-		
-	       item1.addActionListener(new ActionListener() {
-	            public void actionPerformed(ActionEvent listnerP) {
-	            	
-	            	if (listnerP.getActionCommand().equals("Pacman")) {
-	            		System.out.println("you chose pacman.");
-	            		choice = 1; 
-	            	}
 
-	            }});
-	       
-	       item2.addActionListener(new ActionListener() {
-	            public void actionPerformed(ActionEvent listnerF) {
-	            	
-	            	if (listnerF.getActionCommand().equals("Fruit")) {
-	            		System.out.println("you chose fruit.");
-	            		choice = 2; 
-	            	}
+		item1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent listnerP) {
 
-	            }});
+				if (listnerP.getActionCommand().equals("Pacman")) {
+					System.out.println("you chose pacman.");
+					choice = 1; 
+				}
 
-	       
+			}});
+
+		item2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent listnerF) {
+
+				if (listnerF.getActionCommand().equals("Fruit")) {
+					System.out.println("you chose fruit.");
+					choice = 2; 
+				}
+
+			}});
+
+		item3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent listnerPlay) {
+
+				if (listnerPlay.getActionCommand().equals("Play")) {
+					System.out.println("you chose play.");
+					choice = 3; 
+				}
+
+			}});
+
+		item4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent listenClear) {
+
+				if (listenClear.getActionCommand().equals("Clear")) {
+					System.out.println("you chose clear.");
+					choice = 4; //clears the array lists and the photo 
+					//	            		pacman.clear();
+					//	        			fruit.clear();
+					repaint();
+				}
+
+			}});
+
+		item5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				JFileChooser chooser = new JFileChooser();
+				chooser.setFileFilter(new FileNameExtensionFilter("CSV file", "csv"));//filters the files that shown just to csv files
+				chooser.showOpenDialog(null);
+				File chosenFile = chooser.getSelectedFile();
+
+				Game game = new Game(chosenFile.getPath());
+				choice = 5;
+repaint();
+				//לשלוח את הכתובת של הפייל שקיבלנו למפה
+			}
+		});
+
+
 		try {
-			 myImage = ImageIO.read(new File("C:\\Users\\micha\\eclipse-workspace\\cont\\Ariel.jpg"));
+			myImage = ImageIO.read(new File("C:\\Users\\micha\\eclipse-workspace\\cont\\Ariel.jpg"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}		
 	}
+	
+	Map map = new map(myImage);//להגדיר שלא מקבל קאורדינאטות ולהזיז
 
-//	int x = -1;
-//	int y = -1;
+
+	//	int x = -1;
+	//	int y = -1;
 
 	public void paint(Graphics g)
 	{
 		g.drawImage(myImage,0,0,this.getWidth(),this.getHeight(),this); // resizing the picture 
 		//g.drawImage(myImage, 0, 0, this);
-	
-		Iterator<Point3D> pac_iterator = pacman.iterator();
-		Iterator<Point3D> fruit_iterator = fruit.iterator();
+
+		Iterator <Packman> it = game.getPackmans().iterator();
+		Iterator<fruit> it2 = game.getFruits().iterator();
+
+		//		Iterator<Point3D> pac_iterator = pacman.iterator();
+		//		Iterator<Point3D> fruit_iterator = fruit.iterator();
 		int rP= 30;
 		int rF = 20;
 		Color pacman = new Color(255, 255, 113); // Color white
 		Color fruit = new Color(255, 102, 102); // red color for fruit
 
-		
-		while(pac_iterator.hasNext()) {
-			Point3D onPoint = pac_iterator.next();
-			int pX = (int)onPoint.x();
-			int pY = (int)onPoint.y();
-			g.setColor(pacman);
-			g.fillOval(pX,pY,rP,rP);
+		if(choice!=4) {
+			while(it.hasNext()) {
+				Packman p = it.next();
+				Point3D pp = p.getGps();
+				Point3D npp = map.coords2pics(pp);
+				int pX = (int)npp.x();
+				int pY = (int)npp.y();
+				g.setColor(pacman);
+				g.fillOval(pX,pY,rP,rP);
+			}
+
+			while(it2.hasNext()) {
+				fruit f =it2.next();
+				Point3D pf = f.getGps();
+				Point3D npf = map.coords2pics(pf);
+				int fX = (int)npf.x();
+				int fY = (int)npf.y();
+				g.setColor(fruit);
+				g.fillOval(fX,fY,rF,rF);
+
+			}
 		}
 
-		while(fruit_iterator.hasNext()) {
-			Point3D onPoint = fruit_iterator.next();
-			int pX = (int)onPoint.x();
-			int pY = (int)onPoint.y();
-			g.setColor(fruit);
-			g.fillOval(pX,pY,rF,rF);
-		
-		}
-	
-		
-		
-//		if(x!=-1 && y!=-1)
-//		{
-//			int r = 10;
-//			x = x - (r / 2);
-//			y = y - (r / 2);
-//			g.fillOval(x, y, r, r);
-//		}
+
+		//		if(x!=-1 && y!=-1)
+		//		{
+		//			int r = 10;
+		//			x = x - (r / 2);
+		//			y = y - (r / 2);
+		//			g.fillOval(x, y, r, r);
+		//		}
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent arg) {
 		System.out.println("mouse Clicked");
 		System.out.println("("+ arg.getX() + "," + arg.getY() +")");
-//		int x = arg.getX();
-//		int y = arg.getY();
 
-	int pointX= arg.getX();
-	int pointY= arg.getY();
+		int pointX= arg.getX();
+		int pointY= arg.getY();
 		if(choice == 1) {
-		pacman.add(new Point3D(pointX,pointY,0));	
+
+			Point3D p = new Point3D(pointX,pointY,0);
+			Point3D p2c= map.pics2coords(p);
+			Packman pac = new Packman (p2c,1,1);
+			game.getPackmans().add(pac);
+
+
+			//	pacman.add(new Point3D(pointX,pointY,0));	
 		}
-		
+
 		if(choice == 2) {
-			fruit.add(new Point3D(pointX,pointY,0));	
+
+			Point3D p = new Point3D(pointX,pointY,0);
+			Point3D p2c= map.pics2coords(p);
+			fruit fr = new fruit (p2c,1);
+			game.getFruits().add(fr);
+
+			//fruit.add(new Point3D(pointX,pointY,0));	
 		}
-		
+
+		if(choice == 3) {
+			// לקרוא לפונקציה שמחשבת את האלגורתים הקצר ביותר ומחזירה סט של מסלולים. צריך להבין איזה מסלול שייך לאיזה פקמן 
+		}
+
+		if(choice ==4) {
+			game.getFruits().clear();
+			game.getPackmans().clear();
+			//			pacman.clear();
+			//			fruit.clear();
+
+		}
+
 		repaint();
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
 		System.out.println("mouse entered");
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mousePressed(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-
-//	public void resizeWindow(int newWidth,int  newHeight){
-//		System.out.println("inside resize window");
-//		Image img = myImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
-//     
-//	}
-	
-
 
 }
